34395555500347880473d04cf214b0eadee6c4d0
thedivashopper <thedivashopperofficial@gmail.com>
2026-07-17 08:23:42 +0500

    Use non-www as canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 ﻿import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.thedivashopper.com',
-  integrations: [sitemap()],
+  site: 'https://thedivashopper.com',
+  integrations: [],
 });
