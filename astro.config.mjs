1d050c6638c320ed94abdacf71660b2bb68b978c
thedivashopper <thedivashopperofficial@gmail.com>
2026-07-17 08:46:33 +0500

    Fix sitemap and canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 ﻿import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://thedivashopper.com',
-  integrations: [],
+  integrations: [sitemap()],
 });
